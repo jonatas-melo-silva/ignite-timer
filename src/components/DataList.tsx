28dc740819265f20f1074aa96d137c2d792b6096
jonatas-melo-silva <jonatas891024@gmail.com>
2022-07-15 02:16:45 -0300

    :seedling: Add componente com lista de sugestoes

---
+++ src/components/DataList.tsx
@@ -0,0 +1,21 @@
+import { ReactElement } from 'react'
+
+export function DataList(): ReactElement {
+  return (
+    <datalist id="task-suggestions">
+      <option value="React" />
+      <option value="Next.js" />
+      <option value="Node.js" />
+      <option value="Express" />
+      <option value="Typescript" />
+      <option value="Javascript" />
+      <option value="Django" />
+      <option value="Flask" />
+      <option value="Python" />
+      <option value="Java" />
+      <option value="C#" />
+      <option value="Kotlin" />
+      <option value="Dart" />
+    </datalist>
+  )
+}
